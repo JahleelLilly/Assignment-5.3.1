4ddecd4c530a916af160838380008052311dc7be
Jahle <Jahle@ABLACKMAN>
2024-06-06 13:40:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_5._3._1
+{
+    using System;
+
+    class Program
+    {
+        static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+
+            return n <= 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] flowerbed1 = { 1, 0, 0, 0, 1 };
+            Console.WriteLine($"Example 1: {CanPlaceFlowers(flowerbed1, 1)}"); 
+
+            int[] flowerbed2 = { 1, 0, 0, 0, 1 };
+            Console.WriteLine($"Example 2: {CanPlaceFlowers(flowerbed2, 2)}"); 
+        }
+
+    }
+}
